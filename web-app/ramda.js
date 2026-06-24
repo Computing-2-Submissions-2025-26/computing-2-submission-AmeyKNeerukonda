6f41941b08b6e43d47cf99fd31cebf528c0e677a
AmeyKNeerukonda <akn24@ic.ac.uk>
2026-06-24 16:00:13 +0100

    original ramda

--- web-app/ramda.js
+++ web-app/ramda.js
@@ -1,19 +1,7 @@
 /**
- * Integration fix for Ramda.
- * Destructured import to satisfy JSLint.
+ * This is an integration fix, so that Ramda can be imported in the same way
+ * both on server and on browser, and also not trip jslint.
  */
-import {
-    any,
-    assoc,
-    filter,
-    forEach,
-    map
-} from "../node_modules/ramda/es/index.js";
+import * as R from "../node_modules/ramda/es/index.js";
 
-export default Object.freeze({
-    any,
-    assoc,
-    filter,
-    forEach,
-    map
-});
+export default R;
